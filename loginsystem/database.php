ebd37670a341325274fc610308e60ddc8709a3c5
dv1n33 <6057391@mborijnland.nl>
2026-06-10 09:08:24 +0200

    login systeem

---
+++ loginsystem/database.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$db = "users";
+$user = "root";
+$pass = "root";
+
+$conn = new mysqli($host, $user, $pass);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
+
+
